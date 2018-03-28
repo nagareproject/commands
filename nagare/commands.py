a59c69336f1b717f6e3567a5c8bc54646c0625fc
Alain Poirier <alain.poirier@net-ng.com>
2018-03-28 15:52:18 +0200

    Simplications

--- nagare/commands.py
+++ nagare/commands.py
@@ -14,13 +14,46 @@ import argparse
 from nagare.services import plugin, plugins
 
 
-class Commands(plugins.Plugins, plugin.Plugin):
+class Command(plugin.Plugin):
+    def __init__(self, name=None, dist=None, **config):
+        super(Command, self).__init__(name if name is not None else os.path.basename(sys.argv[0]), dist, **config)
+
+    def __call__(self, names, args):
+        return self, names + (self.name,), args
+
+    def set_arguments(self, parser):
+        """Define the available options for this command
+       """
+        pass
+
+    def parse(self, names, args):
+        """Parse the command line
+        """
+        parser = argparse.ArgumentParser(prog=' '.join(names), description=self.DESC)
+        self.set_arguments(parser)
+
+        return vars(parser.parse_args(args))
+
+    @staticmethod
+    def run(**args):
+        return 0
+
+    def _run(self, **args):
+        return self.run(**args)
+
+    def execute(self, args=None):
+        command, names, args = self((), args if args is not None else sys.argv[1:])
+
+        return 2 if command is None else command._run(**command.parse(names, args))
+
+
+class Commands(plugins.Plugins, Command):
     DESC = '<subcommands>'
 
-    def __init__(self, name, dist, entry_points):
+    def __init__(self, name=None, dist=None, entry_points=None):
         self.entry_points = entry_points
         plugins.Plugins.__init__(self, {}, entry_points=entry_points)
-        plugin.Plugin.__init__(self, name, dist)
+        Command.__init__(self, name, dist)
 
     def _load_plugin(self, name, dist, plugin, initial_config, config):
         return super(Commands, self)._load_plugin(
@@ -28,33 +61,8 @@ class Commands(plugins.Plugins, plugin.Plugin):
             entry_points=self.entry_points + '.' + name
         )
 
-    '''
-    def validate(self, args, services_service):
-        sub_commands = {
-            name: subcommand
-            for name, subcommand in self.items()
-            if services_service(subcommand.validate, args)
-        }
-
-        self.clear()
-        self.update(sub_commands)
-
-        return bool(self)
-    '''
-
-    def get_command_names(self, names):
-        return names + (self.name,)
-
-    def find_subcommand(self, names, args):
-        command_names = self.get_command_names(names)
-
-        args = args or [None]
-        command = self.get(args[0])
-
-        return (command_names, self, None) if command is None else command.find_subcommand(command_names, args[1:])
-
-    def parse(self, command_name, args):
-        print 'Usage: ' + command_name + (' <command>' if self else '')
+    def usage(self, names, _):
+        print 'Usage: ' + ' '.join(names) + (' <command>' if self else '')
         if self:
             print
             print 'with <command>:'
@@ -63,54 +71,13 @@ class Commands(plugins.Plugins, plugin.Plugin):
             for name, sub_command in sorted(self.items()):
                 print ' - %s: %s' % (name.ljust(name_max_len), sub_command.DESC)
 
-        return None
+        return None, None, None
+
+    def __call__(self, names, args):
+        subcommand = args.pop(0) if args else None
+
+        return self.get(subcommand, self.usage)(names + (self.name,), args)
 
 
-class ConsoleScript(Commands):
-    def __init__(self, entry_points, script_name=None):
-        if script_name is None:
-            script_name = os.path.basename(sys.argv[0])
-
-        super(ConsoleScript, self).__init__(script_name, None, entry_points)
-
-    def find_subcommand(self, args):
-        names, sub_command, args = super(ConsoleScript, self).find_subcommand((self.name,), args)
-        return ' '.join(names), sub_command, args
-
-    def get_command_names(self, names):
-        return names
-
-    def __call__(self, args=None):
-        if args is None:
-            args = sys.argv[1:]
-
-        command_name, command, args2 = self.find_subcommand(args)
-        arguments = command.parse(command_name, args2)
-
-        return (command, arguments) if arguments is not None else (None, None)
-
-
-class Command(plugin.Plugin):
-    """The base class of all the commands"""
-    DESC = ''
-
-    def find_subcommand(self, names, args):
-        return names + (self.name,), self, args
-
-    '''
-    def validate(self, args):
-        return True
-    '''
-
-    def set_arguments(self, parser):
-        """Define the available options for this command
-       """
-        pass
-
-    def parse(self, command_name, args):
-        """Parse the command line
-        """
-        parser = argparse.ArgumentParser(prog=command_name, description=self.DESC)
-        self.set_arguments(parser)
-
-        return vars(parser.parse_args(args))
+def run(entry_points, commands=Commands):
+    return commands(entry_points=entry_points).execute()
